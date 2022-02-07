d5242682f749fe495e37bbc8c7b20a4b6d251504
Guy Sloan <sales@rackprodigy.com>
2022-02-07 13:50:24 +0000

    ran rust formatter

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,11 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::Write; // <--- bring flush() into scope
-use std::collections::HashMap;
 
 struct Command<'a> {
-   name:&'a str,
-   id:u32,
-   callback:fn() -> Result<String, String>
+    name: &'a str,
+    id: u32,
+    callback: fn() -> Result<String, String>,
 }
 
 pub type CommandResult = Result<String, String>;
@@ -28,24 +28,26 @@ trait Executable {
 }
 
 //implement the trait
-impl <'a> Printable for Command<'a> {
+impl<'a> Printable for Command<'a> {
     fn print(&self) {
-        println!("Printing command usage with id:{} and name {}",self.id,self.name)
+        println!(
+            "Printing command usage with id:{} and name {}",
+            self.id, self.name
+        )
     }
 }
 
-impl <'a> Executable for Command<'a> {
+impl<'a> Executable for Command<'a> {
     fn execute(&self) -> CommandResult {
         (self.callback)()
     }
 }
 
 pub fn create_cli() {
-    init_cli(); 
+    init_cli();
 }
 
-pub fn destroy_cli() {
-}
+pub fn destroy_cli() {}
 
 fn init_cli() {
     let mut commands: HashMap<&str, Command> = HashMap::new();
@@ -53,14 +55,22 @@ fn init_cli() {
     command_loop(&mut commands);
 }
 
-fn register_commands(commands : &mut HashMap<&str, Command>) {
-    let c1 = Command {name:"test", id:0, callback:test};
-    let c2 = Command {name:"exit", id:1, callback:exit};
+fn register_commands(commands: &mut HashMap<&str, Command>) {
+    let c1 = Command {
+        name: "test",
+        id: 0,
+        callback: test,
+    };
+    let c2 = Command {
+        name: "exit",
+        id: 1,
+        callback: exit,
+    };
     commands.insert(c1.name, c1);
     commands.insert(c2.name, c2);
 }
 
-fn command_loop(commands : &HashMap<&str, Command>) {
+fn command_loop(commands: &HashMap<&str, Command>) {
     loop {
         println!("");
         display_prompt();
@@ -68,11 +78,10 @@ fn command_loop(commands : &HashMap<&str, Command>) {
         let data = get_user_input();
         let mut token_vec = Vec::new();
         tokenise_line(data, &mut token_vec);
-        if token_vec.len() > 0
-        {
+        if token_vec.len() > 0 {
             let res = process_command(&commands, &token_vec);
             match res {
-                 Ok(o) => println!("OK: {:?}", o),
+                Ok(o) => println!("OK: {:?}", o),
                 Err(e) => println!("ERROR: {:?}", e),
             }
         }
@@ -90,10 +99,8 @@ fn get_user_input() -> String {
     return line;
 }
 
-fn tokenise_line(line:String, token_vec: &mut Vec<String>) {
-
-    if line.chars().count() > 0
-    {
+fn tokenise_line(line: String, token_vec: &mut Vec<String>) {
+    if line.chars().count() > 0 {
         for token in line.trim().split_whitespace() {
             token_vec.push(token.to_string());
         }
@@ -102,12 +109,7 @@ fn tokenise_line(line:String, token_vec: &mut Vec<String>) {
 
 fn process_command(commands: &HashMap<&str, Command>, token_vec: &Vec<String>) -> CommandResult {
     match commands.get(&token_vec[0] as &str) {
-        Some(command) => { 
-            Ok((command.callback)().unwrap())
-        }
-        None => {
-            Err("command not found".to_string())
-        }
+        Some(command) => Ok((command.callback)().unwrap()),
+        None => Err("command not found".to_string()),
     }
 }
-
